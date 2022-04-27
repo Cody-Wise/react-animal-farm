0195642f3ff27499527d109a872e473a9a16c8aa
Cody-Wise <cody@sharptackmedia.com>
2022-04-26 19:53:24 -0700

    Forgot to add the components for animal and animal list in separate folders

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import backgroundImg from '../background.png';
 import './Main.css';
-import AnimalList from '../AnimalList.js';
+import AnimalList from '../AnimalList/AnimalList';
 
 export default function Main({ animals }) {
   return (
-    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-
-      {
-        animals.map((animal) =>
-
-          <AnimalList
-            key={animal.name}
-            {...animal}/>
-
-        )
-      }
-
-
+    <main style={{ backgroundImage: 'url(background.png)' }}>
+      <AnimalList animals={animals} />
     </main>
   );
-
 }
